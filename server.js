198f8a6501f42ebe566cdcb75714b0c118c32f4e
Sanket Mistry <sanket.mern.dev@gmail.com>
2025-07-03 23:29:17 +0530

    move app.js code

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
-const app = require("./app");
+
 const { sequelize } = require("./models");
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const paymentRoutes = require("./routes/payment.route");
+const planRoutes = require("./routes/plan.route");
+
+// Initialize Express app
+const app = express();
+
+// Middlewares
+app.use(helmet());
+app.use(cors({ origin: '*' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+// Routes
+app.use("/api/payment", paymentRoutes);
+app.use("/api/plans", planRoutes);
+
+module.exports = app;
 
 const PORT = process.env.PORT || 5000;
 
